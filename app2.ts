0b4057da1fbb42c9dccb0305b26c9c58f272182f
Eder do Carmo Almeida <ederdoalmeida@hotmail.com>
2020-09-08 21:20:03 -0300

    6

--- app2.ts
+++ app2.ts
@@ -21,10 +21,64 @@ class Carro {
 
     }
 }
-let carroA = new Carro('Goleta', 5)
-console.log(carroA)
-carroA.acelerar()
-console.log(carroA)
-carroA.acelerar()
-carroA.acelerar()
-console.log(carroA)
+class Concessionaria {
+    private endereco: string
+    private listaDeCarros: Array<Carro>
+
+    constructor (endereco: string, listaDeCarros: Array<Carro>){
+        this.endereco = endereco
+        this.listaDeCarros = listaDeCarros
+    }
+
+    public fornecerEndereco(): string {
+        return this.endereco
+    }
+
+    public mostrarListaDeCarros(): Array<Carro> {
+        return this.listaDeCarros
+    }
+}
+class Pessoa{
+    private nome: string
+    private carroPreferido: string
+    private carro: any
+
+    constructor(nome: string, carroPreferido: string){
+        this.nome = nome
+        this.carroPreferido = carroPreferido
+    }
+    
+    public dizerNome(): string{
+        return this.nome
+    }
+
+    public dizerCarroPreferido(): string{
+        return this.carroPreferido
+    }
+
+    public comparCarro(carro:any): void{
+        this.carro = carro
+    }
+    
+    public dizerCarroQueTem(): any{
+        return this.carro
+    }
+}
+
+
+// criar carros
+let carroA = new Carro('Veloster', 3)
+let carroB = new Carro('Gol', 4)
+let carroC = new Carro('Civic', 4)
+
+// criar array lista de carros
+
+let listaDeCarros: Array<Carro> = [carroA, carroB, carroC]
+let concessionaria = new Concessionaria('Av Paulista', listaDeCarros)
+
+//criar lista de carros
+console.log(concessionaria.mostrarListaDeCarros())
+
+// comprar carro
+let cliente = new Pessoa('Eder', 'Gol')
+console.log(cliente.dizerCarroPreferido())
--- dist/app2.js
+++ dist/app2.js
@@ -16,10 +16,47 @@ var Carro = /** @class */ (function () {
     };
     return Carro;
 }());
-var carroA = new Carro('Goleta', 5);
-console.log(carroA);
-carroA.acelerar();
-console.log(carroA);
-carroA.acelerar();
-carroA.acelerar();
-console.log(carroA);
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco, listaDeCarros) {
+        this.endereco = endereco;
+        this.listaDeCarros = listaDeCarros;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comparCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+// criar carros
+var carroA = new Carro('Veloster', 3);
+var carroB = new Carro('Gol', 4);
+var carroC = new Carro('Civic', 4);
+// criar array lista de carros
+var listaDeCarros = [carroA, carroB, carroC];
+var concessionaria = new Concessionaria('Av Paulista', listaDeCarros);
+//criar lista de carros
+console.log(concessionaria.mostrarListaDeCarros());
+// comprar carro
+var cliente = new Pessoa('Eder', 'Gol');
+console.log(cliente.dizerCarroPreferido());
